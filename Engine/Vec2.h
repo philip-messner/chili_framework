e88b362c417dc51a3ee813c724dcef2e9b27a9d5
Philip Messner <philip.w.messner@gmail.com>
2022-07-20 15:30:26 -0400

    Created Vec2 template class

---
+++ Engine/Vec2.h
@@ -0,0 +1,67 @@
+#pragma once
+
+template<typename T>
+class Vec2
+{
+public:
+	Vec2() = default;
+	Vec2(T x_in, T x_out)
+		:
+		x(x_in),
+		y(y_in)
+	{}
+	Vec2( const Vec2<float>& src)
+		:
+		x((T)src.x),
+		y((T)src.y)
+	{}
+	Vec2 operator+(const Vec2& rhs) const
+	{
+		return Vec2(x + rhs.x, y + rhs.y);
+	}
+	Vec2& operator+=(const Vec2& rhs)
+	{
+		return *this = *this + rhs;
+	}
+	Vec2 operator*(T rhs) const
+	{
+		return Vec2(x * rhs, y * rhs);
+	}
+	Vec2& operator*=(T rhs)
+	{
+		return *this = *this * rhs;
+	}
+	Vec2 operator-(const Vec2& rhs) const
+	{
+		return Vec2(x - rhs.x, y - rhs.y);
+	}
+	Vec2& operator-=(const Vec2& rhs)
+	{
+		return *this = *this + rhs;
+	}
+	T GetLength() const
+	{
+		return (T)std::sqrt(GetLengthSq());
+	}
+	T GetLengthSq() const
+	{
+		return x * x + y * y;
+	}
+	Vec2& Normalize()
+	{
+		return *this = GetNormalized();
+	}
+	Vec2 GetNormalized() const
+	{
+		const T len = GetLength();
+		if (len != (T)0)
+		{
+			return *this * ((T)1 / len);
+		}
+		return *this;
+	}
+
+public:
+	T x;
+	T y;
+};
