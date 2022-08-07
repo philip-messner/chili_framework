03a17030249217fbe5161b9f541e8f2ccdbabdb7
Philip Messner <philip.w.messner@gmail.com>
2022-08-06 21:40:46 -0400

    Added FrameTimer class

---
+++ Engine/FrameTimer.cpp
@@ -0,0 +1,15 @@
+#include "FrameTimer.h"
+
+
+FrameTimer::FrameTimer()
+{
+	last = std::chrono::steady_clock::now();
+}
+
+float FrameTimer::Mark()
+{
+	const auto old = last;
+	last = std::chrono::steady_clock::now();
+	const std::chrono::duration<float> frameTime = last - old;
+	return frameTime.count();
+}
